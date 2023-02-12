24a6d05b004a569b44774f6c1b9d82eb7c541e6a
brywal07 <60237935+brywal07@users.noreply.github.com>
2023-02-12 00:08:09 -0600

    Revert "Merge remote-tracking branch 'origin/main'"
    This reverts commit 6db48b347cd180d33c431d4075b149517ee46679, reversing
    changes made to 9578371723a7fcbdd78a3f1416d8620d9ae99590.

--- app/src/main/java/com/bignerdranch/android/itc362_hw1_ex2/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/itc362_hw1_ex2/MainActivity.kt
@@ -47,6 +47,10 @@ class MainActivity : AppCompatActivity() {
                 Toast.makeText(this, "ERROR!", Toast.LENGTH_SHORT).show()
             }
         }
+        binding.questionTextView.setOnClickListener{
+            currentIndex = (currentIndex + 1) % questionBank.size
+            updateQuestion()
+        }
 
         updateQuestion()
     }
